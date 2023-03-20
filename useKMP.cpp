32cb342d171930cc48b0c65902bb1f800e302d73
mr7aali <mr7aali@gmail.com>
2023-03-20 23:44:58 +0600

    lps array done

--- useKMP.cpp
+++ useKMP.cpp
@@ -2,19 +2,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void useKMP(string pattern){
+
+    int pattern_length = pattern.length();
+    int lps[pattern_length];
+
+    int index =0,tem=0;
+
+    for(int i=1;i < pattern_length;){
+        if(pattern[index]==pattern[i]){
+
+            lps[i]= index + 1;
+           index++;
+           i++;
+
+        }else{
+           if(index != 0 ){
+             index= lps[index-1];
+        
+           }
+
+            else{
+                lps[i]= index;
+                 i++;
+            }
+                
+           
+        }
+    }
 
 
+
+    for(int tem=0;tem<pattern_length;tem++){
+         cout<<lps[tem]<<",";
+    }
+}
 
 int main(){
 
-     string text,pattern;
-    
-    printf("Enter Text : ");
-    cin>>text;
+    string text, pattern="aabaabaaa";
 
-    printf("Enter Pattern : ");
-    cin>>pattern;
+    // printf("Enter Text : ");
+    // cin>>text;
 
+   // printf("Enter Pattern : ");
+  //  cin >> pattern;
+
+    useKMP(pattern);
 
     return 0;
 }
