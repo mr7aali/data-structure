e8e04b77ac24fe0b529a86108d28b1eb5f4a8696
mr7aali <mr7aali@gmail.com>
2023-03-20 22:05:37 +0600

    E

--- useKMP.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-
-    printf("hello word");
-    return 0;
-}
---
+++ useKMP.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+
+     string text,pattern;
+    
+    printf("Enter Text : ");
+    cin>>text;
+
+    printf("Enter Pattern : ");
+    cin>>pattern;
+
+
+    return 0;
+}
