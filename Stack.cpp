3ebf797231ebf3ec122a002a292bc6915842933b
mr7aali <mr7aali@gmail.com>
2023-03-27 23:10:20 +0600

    a

---
+++ Stack.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int top=-1;
+int stk=5;
+
+char stk_store[5];
+
+
+void push(char x){
+
+
+    if(stk==0){
+   // printf("Overflow");
+    }
+    else{
+    top++;
+    stk--;
+
+    stk_store[stk]=x;
+
+    }
+
+}
+
+int main(){
+
+push('0');
+push('1');
+push('2');
+push('3');
+push('4');
+push('5');
+
+
+push('6');
+push('7');
+push('8');
+push('9');
+
+cout<<"String: "<< stk_store<<endl;
+return 0;
+}
