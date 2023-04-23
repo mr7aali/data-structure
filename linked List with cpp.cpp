68b89cdd047a263a17495ddb917126e8536595ef
mr7aali <mr7aali@gmail.com>
2023-04-23 16:10:08 +0600

    li

--- linked List with cpp.cpp
+++ linked List with cpp.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
 
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int value){
+    data = value;
+    next = NULL;
+    }
+
+};
+
+void insertAtTail(Node* &head, int val){
+   Node* n = new Node(val);
+
+   Node* temp = head;
+   while(temp->next != NULL){
+    temp = temp->next;
+   }
+    temp->next=n;
+}
+
+
+int main(){
+
+
+return 0;
+}
