89af34d98080e7d7d75c929b975c9285a68937ae
mr7aali <mr7aali@gmail.com>
2023-05-25 02:41:19 +0600

    binary game

--- Stack.cpp
+++ Stack.cpp
@@ -5,12 +5,13 @@ using namespace std;
 int top=-1;
 int stk=5;
 
-char stk_store[5];
+int stk_store[5];
 
 
-void push(char x){
-    if(stk==0){
+void push(int x){
+    if(stk == 0){
    // printf("Overflow");
+  // return;
     }
     else{
     top++;
@@ -21,19 +22,23 @@ void push(char x){
 
 int main(){
 
-push('0');
-push('1');
-push('2');
-push('3');
-push('4');
-push('5');
+push(0);
+push(1);
+push(2);
+push(3);
+push(4);
+push(5);
 
 
-push('6');
-push('7');
-push('8');
-push('9');
+push(6);
+push(7);
+push(8);
+push(9);
 
-cout<<"String: "<< stk_store<<endl;
+//cout<<"String: "<< stk_store<<endl;
+cout<<"Stack: ";
+for(int i=0;i<5;i++){
+    cout<< stk_store[i];
+}
 return 0;
 }
