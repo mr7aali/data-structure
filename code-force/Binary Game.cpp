1a8bf0a257280b89f362ce2cde0013189bcde4a5
mr7aali <mr7aali@gmail.com>
2023-05-29 23:18:26 +0600

    add

--- code-force/Binary Game.cpp
+++ code-force/Binary Game.cpp
@@ -1,46 +1,28 @@
 #include<iostream>
 using namespace std;
-int arr_stack[100],sum=0;
-int k=-1;
+int sum=0;
 
-void convertToBinary(int decimal){
-
-for(int i=0; ;i++){
-        if(decimal==1){
-        k++;
-        arr_stack[k]=1;
-        sum++;
-        break;
-        }
-        else if(decimal==0){
-            k++;
-            arr_stack[k]=0;
-            break;
-        }
-        k++;
-      arr_stack[k]=decimal%2;
-      sum=sum+decimal%2;
-      decimal = decimal/2;
-
+int convertToBinary(int decimal){
+   if(decimal >1){
+     sum=  sum + decimal % 2;
+   return convertToBinary(decimal/2);
+   }
+   else if(decimal == 0){
+    return 0;
+   }
+   sum++;
 }
-for(int i=0;i<=k;i++){
-    cout<<"S"<<arr_stack[i]<<endl;
+int totalDistance(int D){
+  if(D>=0){
+        convertToBinary(D);
+        D--;
+    return totalDistance(D);
+  }
 }
-cout<<"-----"<<endl;
-}
-
-
 int main(){
-int decimal=5   ;
-
-
-for(decimal;decimal>=0;decimal--){
-    //cout<<decimal<<endl;
-    convertToBinary(decimal);
-}
-
-
-cout<<"K:"<<k<<endl;
-cout<<"Sum :"<<sum<<endl;
+  int distance;
+  cin>>distance;
+ totalDistance(distance);
+cout<<sum<<endl;
 return 0;
 }
