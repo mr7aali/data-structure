0b496863bc08246d5914bd8fc414ff9053d4ab4d
mr7aali <mr7aali@gmail.com>
2023-07-24 02:14:34 +0600

    ex */+ added

--- validExpression.cpp
+++ validExpression.cpp
@@ -3,14 +3,21 @@ using namespace std;
 
 int main(){
 string s_stack="a(bc)*de";
+cout<<"Inter Expression : ";
+cin>>s_stack;
 int k=0;
 
-string text="abcbcde";
+string text="ade";
+cout<<"Inter text : ";
+cin>>text;
+
+
 string temp="";
 
 int text_len = text.size();
 int s_stack_len = s_stack.size();
-int isValid = false;
+int isValid = true;
+
 for(int i=0;i<text_len;i++){
 
   if('(' == s_stack[k]){
@@ -18,8 +25,26 @@ for(int i=0;i<text_len;i++){
         while(')' != s_stack[k]){
         temp.push_back(s_stack[k]);
         k++;
-        } k+=2;
+        }
+
+       if(s_stack[k+1]=='+'){
+            int temp_len = temp.size();
+         for(int j=0;j<temp_len;j++){
+            if(temp[j]==text[i]){
+                i++;
+            } else{
+            isValid = false;
+            break;
+            }
+         }
+       }
+
+
+        k+=2;
 }
+ if(!isValid){
+     break;
+ }
 
    if(s_stack[k] == text[i]){
         k++;
