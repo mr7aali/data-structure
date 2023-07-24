b30a843300ecd6f228459ec83b33fc8574a4fda3
mr7aali <mr7aali@gmail.com>
2023-07-24 11:29:04 +0600

    done

--- validExpression.cpp
+++ validExpression.cpp
@@ -1,123 +1,172 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-//string s_stack="a(bc)*de";
-string s_stack="[a-m]*";
+int main()
+{
+    // string reg_ex="a(bc)*de";
+    string reg_ex = "[^aeiou]";
 
+    int k = 0;
 
-int k=0;
+    string text = "bdefrp";
 
-string text="below";
+    string temp = "";
 
+    int text_len = text.size();
+    int reg_ex_len = reg_ex.size();
+    int isValid = true;
+    int LeftRen = -1;
+    int RightRen = -1;
+    for (int i = 0; i < text_len; i++)
+    {
 
-string temp="";
+        if ('(' == reg_ex[k])
+        {
+            k++;
+            while (')' != reg_ex[k])
+            {
+                temp.push_back(reg_ex[k]);
+                k++;
+            }
 
-int text_len = text.size();
-int s_stack_len = s_stack.size();
-int isValid = true;
-int LeftRen =-1;
-int RightRen = -1;
-for(int i=0;i<text_len;i++){
+            if (reg_ex[k + 1] == '+')
+            {
+                int temp_len = temp.size();
+                for (int j = 0; j < temp_len; j++)
+                {
+                    if (temp[j] == text[i])
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+            if (reg_ex[k + 1] == '?')
+            {
+                int temp_len = temp.size();
+                int subCkh = 0;
+                for (int j = 0; j < temp_len; j++)
+                {
 
-  if('(' == s_stack[k]){
-        k++;
-        while(')' != s_stack[k]){
-        temp.push_back(s_stack[k]);
-        k++;
+                    if ((temp[j] == text[i]))
+                    {
+                        if (text[i] == text[i + temp_len])
+                        {
+                            subCkh++;
+                        }
+
+                        i++;
+                    }
+
+                    if (subCkh >= temp_len)
+                    {
+                        isValid = false;
+                    }
+                }
+            }
+
+            k += 2;
         }
 
-       if(s_stack[k+1]=='+'){
-            int temp_len = temp.size();
-         for(int j=0;j<temp_len;j++){
-            if(temp[j]==text[i]){
+
+       if('['== reg_ex[k] && '^' == reg_ex[k+1]){
+            k +=2;
+          //  string tem_str="";
+          //  int tem_str_len = tem_str.size();
+
+          //  while (']' != reg_ex[k]){
+          //      temp.push_back(tem_str[k]);
+          //      k++;
+          //  }
+           while(']' != reg_ex[k]){
+              if(text[i] == 'a' || text[i] =='e' || text[i] =='i' || text[i]=='o' || text[i]=='u' ){
+                isValid= false ;
+                break;
+              }
+              else{
                 i++;
-            } else{
+                k++;
+              }
+
+           }
+
+
+       }
+
+        if ('[' == reg_ex[k])
+        {
+            k++;
+            LeftRen = reg_ex[k];
+            k += 2;
+            RightRen = reg_ex[k];
+
+            k += 3;
+        }
+
+
+
+
+
+        if (!isValid)
+        {
+            break;
+        }
+
+        if (reg_ex[k] == text[i])
+        {
+            k++;
+        }
+        else if (text[i] == temp[0])
+        {
+            int temp_len = temp.size();
+            for (int j = 0; j < temp_len; j++)
+            {
+                if (text[i] == temp[j])
+                {
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            i--;
+        }
+        else if (LeftRen != -1)
+        {
+            int c_decimal = text[i];
+            if (c_decimal < LeftRen || c_decimal > RightRen)
+            {
+                isValid = false;
+                break;
+            }
+        }
+
+        else
+        {
+            // cout<<"Invalid text"<<endl;
             isValid = false;
             break;
-            }
-         }
-       }
-        if(s_stack[k+1]=='?'){
-        int temp_len = temp.size();
-        int subCkh=0;
-         for(int j=0;j<temp_len;j++){
-
-            if((temp[j]==text[i])){
-                if(text[i]==text[i+temp_len]){
-                    subCkh++;
-                }
-
-                i++;
-            }
-
-         if(subCkh>=temp_len){
-                    isValid=false;
-                }
-         }
         }
-
-
-
-        k+=2;
-}
-   if('[' == s_stack[k]){
-     k++;
-     LeftRen =s_stack[k];
-     k+=2;
-     RightRen =s_stack[k];
-
-      k +=3;
-   }
-
-
- if(!isValid){
-     break;
- }
-
-   if(s_stack[k] == text[i]){
-        k++;
-
-    }
-    else  if (text[i]==temp[0]){
-        int temp_len = temp.size();
-        for(int j=0;j<temp_len;j++){
-            if(text[i]==temp[j]){
-                    i++;
-                continue;
-            }
-            else{
-          //  cout<<"Invalid text"<<endl;
-           isValid=false;
-              break;
-            }
-
-        }
-         i--;
-    }
-    else if(LeftRen != -1){
-        int c_decimal = text[i];
-        if( c_decimal<LeftRen || c_decimal > RightRen ){
-            isValid=false;
-            break;
+        if (((i + 1) == text_len) && (reg_ex_len == k))
+        {
+            isValid = true;
         }
     }
 
-    else{
-       // cout<<"Invalid text"<<endl;
-        isValid=false;
-            break;
+    if (isValid)
+    {
+        cout << "Valid text" << endl;
     }
-if( ((i+1)== text_len) && (s_stack_len==k) ){
-    isValid= true;
-}
-}
-
-if(isValid){
-    cout<<"Valid text"<<endl;
-}
-else{
-    cout<<"Invalid text"<<endl;
-}
-return 0;
+    else
+    {
+        cout << "Invalid text" << endl;
+    }
+    return 0;
 }
