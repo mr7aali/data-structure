d8283454939b1a56d087f7112ef092c2e2465826
mr7aali <mr7aali@gmail.com>
2023-11-06 02:14:01 +0600

    a

--- cpp/graph/Represent/AdjacencyMatrix.cpp
+++ cpp/graph/Represent/AdjacencyMatrix.cpp
@@ -1,25 +1,32 @@
-#include <iostream>
-#include <vector>
-
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int main() {
-    int numVertices = 5; // Number of vertices in the graph
-    vector<  vector<int> > adjacencyMatrix(numVertices, vector<int>(numVertices, 0)   );
 
-    // Adding edges to the graph
-    adjacencyMatrix[0][1] = 1;
-    adjacencyMatrix[0][2] = 1;
-    adjacencyMatrix[1][3] = 1;
-    adjacencyMatrix[2][4] = 1;
+int main(){
+int vertex,edge;
+cin>>vertex>>edge;
 
-    // Printing the adjacency matrix
-    for (int i = 0; i < numVertices; ++i) {
-        for (int j = 0; j < numVertices; ++j) {
-            cout << adjacencyMatrix[i][j] << " ";
-        }
-        cout << endl;
+vector<vector<int>>graph(vertex+1,vector<int>(vertex+1,0));
+
+
+    for(int i=0;i<edge;++i){
+        int v,u;
+        cin>>v>>u;
+        graph[v][u]=1;
+        graph[u][v]=1;
+
     }
 
-    return 0;
+for(int i=0;i<=vertex;i++)graph[0][i]=i;
+for(int i=0;i<=vertex;i++)graph[i][0]=i;
+
+ for(int i=0;i<=vertex;i++){
+    for(int j=0;j<=vertex;j++){
+        cout<<graph[i][j]<<" ";
+    }
+cout<<endl;
+ }
+
+return 0;
 }
