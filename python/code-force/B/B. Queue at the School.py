5def97a0a0eaba42f3c64eef90b9b37442895e67
mr7aali <mr7aali@gmail.com>
2023-11-12 23:47:37 +0600

    code-force a

---
+++ python/code-force/B/B. Queue at the School.py
@@ -0,0 +1,16 @@
+
+n, t = map(int, input().split())
+
+studentQueue = list(input())
+
+
+for t in range(t):
+    i = 0
+    while i < n-1:
+        if studentQueue[i] == "B" and studentQueue[i+1] == "G":
+            studentQueue[i], studentQueue[i + 1] = studentQueue[i + 1], studentQueue[i]
+            i += 2
+        else: i += 1
+
+result = ''.join(studentQueue)
+print(result)
