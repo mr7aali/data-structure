17c837d73323092c4c02d729c5144e4a740b5c9f
mr7aali <mr7aali@gmail.com>
2024-01-10 00:25:02 +0600

    java

---
+++ java/Oop/Oops.java
@@ -0,0 +1,47 @@
+
+class Pen {
+    String color;
+    String type;
+
+    public void write() {
+        System.out.println("writing something...");
+    }
+
+    public void printColor() {
+        System.out.println(this.color);
+    }
+}
+
+class Student {
+    String name;
+    int age;
+
+    public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class Oops {
+    public static void main(String args[]) {
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        Pen pen2 = new Pen();
+        pen2.color = "black";
+        pen2.type = "ballpoint";
+
+        pen1.printColor();
+        pen2.printColor();
+
+        Student s1 = new Student("Mr ali", 24);
+        s1.printInfo();
+
+    }
+}
