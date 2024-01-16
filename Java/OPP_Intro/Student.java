7f11aa5422ccd2494bf864b49aa0e0e9f189ddcd
mr7aali <mr7aali@gmail.com>
2024-01-16 14:09:09 +0600

    constrauctor overloading

--- Java/OPP_Intro/Student.java
+++ Java/OPP_Intro/Student.java
@@ -1,6 +1,6 @@
 package Java.OPP_Intro;
 
-import java.util.Arrays;
+
 
 /**
  * Student
@@ -9,6 +9,12 @@ public class Student {
     int rno;
     String name;
     float marks;
+
+    Student() {
+        this.rno = 1;
+        this.name = "Mr Ali";
+        this.marks = 80.5f;
+    }
 
     Student(int rno, String name, float marks) {
         this.rno = rno;
@@ -26,16 +32,21 @@ public class Student {
 
     void getStudentInfo() {
         System.out.println(
-                "Hello! My name is" + this.name + ", my Roll number is " + this.rno + "and a I get " + this.marks);
+                "Hello! My name is <" + this.name + ">, my Roll number is <" + this.rno + "> and a I get <" + this.marks +">");
     }
 }
 
 class Main {
     public static void main(String[] args) {
         Student[] students = new Student[5];
+
         Student Fardin = new Student(28, "ali", 80.5f);
-        Fardin.changeName("Ali");
+        Fardin.changeName("Fardin Khan");
         Fardin.greeting();
         Fardin.getStudentInfo();
+
+        Student Ali = new Student();
+        
+        Ali.getStudentInfo();
     }
 }
