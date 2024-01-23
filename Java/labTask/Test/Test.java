1cbd1dcbf3028d4793f5ac0569059a1e0b01ba0f
mr7aali <mr7aali@gmail.com>
2024-01-23 11:31:00 +0600

    s

---
+++ Java/labTask/Test/Test.java
@@ -0,0 +1,10 @@
+package Java.labTask.Test;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 20.2;
+        String c = "Hello";
+        System.out.println(a + b + c);
+    }
+}
