ace016a7fc583a7dfd873fc2ddaf6b67a15c3b84
mr7aali <mr7aali@gmail.com>
2024-02-11 23:29:35 +0600

    a

---
+++ Java/Test/DymondProblem.java
@@ -0,0 +1,21 @@
+package Java.Test;
+
+class A {
+    void display() {
+        System.out.println("Display function called !");
+    }
+
+}
+
+class B extends A {
+}
+
+class C extends A {
+}
+
+class D extends B {
+}
+
+public class DymondProblem {
+
+}
