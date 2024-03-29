f65ed9d74bd9573be79bcff333a49cff6b1f321f
mr7aali <mr7aali@gmail.com>
2024-03-30 02:57:03 +0600

    assignment 2 done

--- Java/FinalExam/assignment/assignment_2/assignment_2.java
+++ Java/FinalExam/assignment/assignment_2/assignment_2.java
@@ -1,5 +1,83 @@
 package Java.FinalExam.assignment.assignment_2;
 
+class Bicycle {
+    double frameSize;
+    int nGears;
+    int currentGear;
+
+    // Bicycle(){}
+    Bicycle(double frameSize, int nGears) {
+        this.frameSize = frameSize;
+        this.nGears = nGears;
+    }
+}
+
+class CargoBicycle extends Bicycle {
+    double maxLoad;
+    double currentLoad;
+
+    CargoBicycle(double frameSize, int nGears, double maxLoad) {
+        super(frameSize, nGears);
+        this.maxLoad = maxLoad;
+    }
+}
+
+interface Electrified {
+    int getChargeCapacity();
+
+    double getCurrentCharge();
+
+    void setCurrentCharge(double charge);
+
+}
+
+class ElectricBicycle extends Bicycle implements Electrified {
+    int batteryCapacity;
+    double currentCharge;
+
+    ElectricBicycle(double frameSize, int nGears, int batteryCapacity) {
+        super(frameSize, nGears);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public int getChargeCapacity() {
+        return batteryCapacity;
+    }
+
+    public double getCurrentCharge() {
+        return currentCharge;
+    }
+
+    public void setCurrentCharge(double charge) {
+        currentCharge = charge;
+    }
+}
+
+class ElectricCargoBicycle extends CargoBicycle implements Electrified {
+    int batteryCapacity;
+    double currentCharge;
+
+    ElectricCargoBicycle(double frameSize, int nGears, double maxLoad, int batteryCapacity) {
+        super(frameSize, nGears, maxLoad);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public int getChargeCapacity() {
+        return batteryCapacity;
+    }
+
+    public double getCurrentCharge() {
+        return currentCharge;
+    }
+
+    public void setCurrentCharge(double charge) {
+        currentCharge = charge;
+    }
+}
+
 public class assignment_2 {
-    
+    public static void main(String[] args) {
+        ElectricCargoBicycle Cargo = new ElectricCargoBicycle(26, 6, 90, 900);
+
+    }
 }
